dd0811e3125cfe003b491929f60670061b08dd7b
shivam sharma <shivamdocs0@gmail.com>
2025-08-16 22:39:54 +0530

    Refactor app.js for improved readability and consistency in formatting

--- src/app.js
+++ src/app.js
@@ -1,17 +1,42 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-const app=express()
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    cridentials:true
-}))
- app.use(express.json({limit:"16kb"}))//put the data into req.body
- app.use(express.urlencoded({extended:true,limit:"16kb"}))//parse form data 
- app.use(express.static("public"))//user can see the files from frontend
-app.use(cookieParser())
-// routes
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//routes import
 import userRouter from './routes/user.routes.js'
-app.use('/api/v1/users',userRouter)//'/user
-export {app};
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+export { app }
